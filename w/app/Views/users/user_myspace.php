e5808042749e3e1c74c7b012856a4edb64eaab73
L3h4ne <lise.n@hotmail.fr>
2017-07-03 12:32:58 +0200

    suite modifs user space
    :+1:  rien de particulier, des modifs

--- w/app/Views/users/user_myspace.php
+++ w/app/Views/users/user_myspace.php
@@ -11,67 +11,68 @@
 
 <form method='POST' action='#'>
 	<div class='container-fluid'>
-		<div class='col-md-4'>
-			<label for='email'>Email</label>
+		<div class='row'>
+			<div class='col-md-4'>
+				<label for='email'>Email</label>
+			</div>
+			<div class='col-md-8'>
+				<input type='email' name='email' value="<?= $_SESSION['email']?>">
+			</div>
 		</div>
-		<div class='col-md-8'>
-			<input type='email' name='email' value="<?= $_SESSION['email']?>">
-		</div>
-	</div>
 
+		<div class='row'>
+			<div class='col-md-4'>
+				<label for='level'>Niveau</label>
+			</div>
+			<div class='col-md-8'>
+				<select name='level'>
+					<!-- Le php sert à sélectionner le bon niveau pour l'utilisateur s'il l'a déjà renseigné -->
+					<option value='1' <?php if(isset($_SESSION['level']) && $_SESSION['level'] == 1){ echo 'selected'; }?>>Débutant, spécialiste du air ball</option>
+					<option value='2' <?php if(isset($_SESSION['level']) && $_SESSION['level'] == 2){ echo 'selected'; }?>>Novice, je débute mais j'arrive à toucher le panier </option>
+					<option value='3' <?php if(isset($_SESSION['level']) && $_SESSION['level'] == 3){ echo 'selected'; }?>>Intermédiaire, je me débrouille</option>
+					<option value='4'<?php if(isset($_SESSION['level']) && $_SESSION['level'] == 4){ echo 'selected'; }?>>Avancé, ça fait des années que je joue</option>
+					<option value='5'<?php if(isset($_SESSION['level']) && $_SESSION['level'] == 5){ echo 'selected'; }?>>Expert, j'ai raté une carrière à la NBA</option>
+				</select>
+			</div>
+		</div>
+		<div class='row'>
+			<div class='col-md-4'>
+				<label for='address'>Adresse</label>
+			</div>
+			<div class='col-md-8'>
+				<textarea type='text' name='address' value="<?= $_SESSION['address']?>"></textarea>
+			</div>
+		</div>
 
-	<div class='row'>
-		<div class='col-md-4'>
-			<label for='level'>Niveau</label>
+		<div class='row'>
+			<div class='col-md-4'>
+				<label for='cp'>Code Postal</label>
+			</div>
+			<div class='col-md-8'>
+				<input type='text' name='cp' value="<?= $_SESSION['cp']?>">
+			</div>
 		</div>
-		<div class='col-md-8'>
-			<select name='level'>
-				<option value='1'>Débutant, spécialiste du air ball</option>
-				<option value='2'>Novice, je débute mais j'arrive à toucher le panier </option>
-				<option value='3'>Intermédiaire, je me débrouille</option>
-				<option value='4'>Avancé, ça fait des années que je joue</option>
-				<option value='5'>Expert, j'ai raté une carrière à la NBA</option>
-			</select>
-		</div>
-	</div>
-	<div class='row'>
-		<div class='col-md-4'>
-			<label for='address'>Adresse</label>
-		</div>
-		<div class='col-md-8'>
-			<textarea type='text' name='address' value="<?= $_SESSION['address']?>"></textarea>
-		</div>
-	</div>
 
-	<div class='row'>
-		<div class='col-md-4'>
-			<label for='cp'>Code Postal</label>
+		<div class='row'>
+			<div class='col-md-4'>
+				<label for='city'>Ville</label>
+			</div>
+			<div class='col-md-8'>
+				<input type='text' name='city' value="<?= $_SESSION['city']?>">
+			</div>
 		</div>
-		<div class='col-md-8'>
-			<input type='text' name='cp' value="<?= $_SESSION['cp']?>">
-		</div>
-	</div>
 
-	<div class='row'>
-		<div class='col-md-4'>
-			<label for='city'>Ville</label>
+		<div class='row'>
+			<div class='col-md-4'>
+				<label for='phone'>Téléphone (facultatif)</label>
+			</div>
+			<div class='col-md-8'>
+				<input type='text' name='phone' value="<?php if(isset($_SESSION['phone'])){ echo $_SESSION['phone'];}?>">
+			</div>
 		</div>
-		<div class='col-md-8'>
-			<input type='text' name='city' value="<?= $_SESSION['city']?>">
-		</div>
+		<br>
+		<button type='submit' class='btn btn-primary'>Envoyer les modifications</button>
 	</div>
-
-	<div class='row'>
-		<div class='col-md-4'>
-			<label for='phone'>Téléphone (facultatif)</label>
-		</div>
-		<div class='col-md-8'>
-			<input type='text' name='phone' value="<?php if(isset($_SESSION['phone'])){ echo $_SESSION['phone'];}?>">
-		</div>
-	</div>
-	<br>
-	<button type='submit' class='btn btn-primary'>Envoyer les modifications</button>
-
 
 
 </form>
@@ -91,7 +92,7 @@
 				<div class='col-md-4'>
 					<label for='name'>Nom</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<input type='text' name='name' placeholder="Un nom pour ce terrain">
 				</div>
 			</div>
@@ -100,7 +101,7 @@
 				<div class='col-md-4'>
 					<label for='description'>Description</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<textarea type='text' name='description' placeholder='Une description du terrain et  des infrastructures disponibles' rows='10'></textarea>
 				</div>
 			</div>
@@ -109,11 +110,11 @@
 				<div class='col-md-4'>
 					<label for='court_state'>Etat du terrain</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<select name='level'>
 						<option value='very_bad'>Très mauvais état !</option>
 						<option value='bad'>Mauvais état </option>
-						<option value='medium'>Etat moyen, acceptable </option>
+						<option value='medium' selected>Etat moyen, acceptable </option>
 						<option value='good'>Bon état </option>
 						<option value='very_good'>Très bon état !</option>
 					</select>
@@ -124,7 +125,7 @@
 				<div class='col-md-4'>
 					<label for='address'>Adresse</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<textarea type='text' name='address' placeholder="Adresse du terrain"></textarea>
 				</div>
 			</div>
@@ -133,7 +134,7 @@
 				<div class='col-md-4'>
 					<label for='postal_code'>Code Postal</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<input type='text' name='postal_code' placeholder="CP" ">
 				</div>
 			</div>
@@ -142,7 +143,7 @@
 				<div class='col-md-4'>
 					<label for='city'>Ville</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<input type='text' name='city' placeholder="Ville">
 				</div>
 			</div>
@@ -151,14 +152,14 @@
 				<div class='col-md-4'>
 					<label for='city'>Photo</label>
 				</div>
-				<input type='file' name='picture' class='col-md-4'>
+				<input type='file' name='picture' class='col-md-8'>
 			</div>
 
 			<div class='row'>
 				<div class='col-md-4'>
 					<label for='net'>Filet sur le(s) panier(s) ?</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<input type='radio' name='net' value='yes'> Oui -  
 					<input type='radio' name='net' value='no'> Non
 				</div>
@@ -168,7 +169,7 @@
 				<div class='col-md-4'>
 					<label for='opening_hours'>Horaires d'ouverture (facultatif)</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<input type='text' name='opening_hours' placeholder="Horaires d'ouverture">
 				</div>
 			</div>
@@ -177,7 +178,7 @@
 				<div class='col-md-4'>
 					<label for='parking'>Parking (facultatif)</label>
 				</div>
-				<div class='col-md-4'>
+				<div class='col-md-8'>
 					<input type='radio' name='parking' value='yes'> Oui -  
 					<input type='radio' name='parking' value='no'> Non
 				</div>
