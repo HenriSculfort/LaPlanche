5edff2b4c64ee6731b2eff2084a0f68b47acbf60
nlogeais <logeaisnicolas@yahoo.fr>
2017-07-03 10:42:05 +0200

    Evolution page index

--- w/app/Views/default/index.php
+++ w/app/Views/default/index.php
@@ -1,6 +1,16 @@
 <?=$this->layout('layout', ['title' => 'Accueil']); ?>
 
 
-<?php $this->start('main_content') ?>
+<?php $this->start('searchIndex_content') ?>
 
-<?php $this->stop('main_content') ?>
+<input type="text" name="whereSearch">
+<button type="button" class="btn btn-warning" placeholder="<i class='fa fa-search' aria-hidden='true'></i>">Rechercher</button>
+
+
+<?php $this->stop('searchIndex_content') ?>
+
+
+
+<?php $this->start('footer_content') ?>
+
+<?php $this->stop('footer_content') ?>
--- w/app/Views/layout.php
+++ w/app/Views/layout.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="<?= $this->assetUrl('css/clean-blog.min.css') ?>">
 
     <!-- Custom Fonts -->
-    <link href="vendor/fortawesome/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= $this->assetUrl('font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
@@ -69,14 +69,14 @@
 
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url(<?= $this->assetUrl('img/home-bg.jpg') ?>)">
+    <header class="intro-header" style="background-image: url(<?= $this->assetUrl('img/sport-ground.jpg') ?>)">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
                         <h1>La Planche</h1>
                         <hr class="small">
-                        <span class="subheading">Un projet de batard !! </span>
+                        <?= $this->section('searchIndex_content') ?>
                     </div>
                 </div>
             </div>
