152d7065084c2e2046adf64c36245c35e7f3c2a8
EBisbibi <etienne.braud@hotmail.fr>
2017-07-07 16:23:02 +0200

    protec espace admin

--- w/app/Views/admin/compte.php
+++ w/app/Views/admin/compte.php
@@ -10,69 +10,69 @@
 
 <?=$this->start('main_content'); ?>
 <?php
-//if($_SESSION['role'] = 'admin'){
-?>
-<article>
-	<div id="errors" style="color:red"></div>
-	<div id="message" style="color:green"></div>
-</article>
+if(!$_SESSION['role'] = 'admin'){
+	?>
+	<article>
+		<div id="errors" style="color:red"></div>
+		<div id="message" style="color:green"></div>
+	</article>
 
-<div>
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th>Pseudo utilisateur</th>
-				<th>Niveau</th>
-				<th>Prénom</th>
-				<th>Nom</th>
-				<th>email</th>
-				<th>adresse</th>
-				<th>Téléphone</th>
-				<th>Statut</th>
-				<th>Supprimer</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			foreach ($listuser as $key => $value) { ?>
-			<tr>
+	<div>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Pseudo utilisateur</th>
+					<th>Niveau</th>
+					<th>Prénom</th>
+					<th>Nom</th>
+					<th>email</th>
+					<th>adresse</th>
+					<th>Téléphone</th>
+					<th>Statut</th>
+					<th>Supprimer</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				foreach ($listuser as $key => $value) { ?>
+				<tr>
 
-				<form type="GET" id="user-id-<?= $value['id']; ?>">
-					<td><?php echo $value['username'];?></td>
-					<td><?php echo $value['level'];?></td>
-					<td><?php echo $value['firstname'];?></td>
-					<td><?php echo $value['lastname'];?></td>
-					<td><?php echo $value['email'];?></td>
-					<td><?php echo $value['address'] . ' '. $value['postal_code'] . ' ' . $value['city'];?></td>
-					<td><?php echo $value['phone'];?></td>
-					<td>
-						<select name="role" class="select-role" id="role-<?= $value['id']; ?>" >
-							<option value="user" <?php if(isset($value['role']) && $value['role'] == 'user'){ echo 'selected'; }?>>User</option>
-							<option value="admin" <?php if(isset($value['role']) && $value['role'] == 'admin'){ echo 'selected'; }?>>Admin</option>
-						</select>
-					</td>
-					<td>
-						<input type="checkbox" name="suppr" id="suppr-<?= $value['id']; ?>">
-					</td>
-					
-					<td>
-						<button type="submit" data-id="<?=$value['id'];?>">Appliquez</button>
-					</td>
-				</form>
-			</tr>
-			<?php }	?>
-		</tbody>
-	</table>
+					<form type="GET" id="user-id-<?= $value['id']; ?>">
+						<td><?php echo $value['username'];?></td>
+						<td><?php echo $value['level'];?></td>
+						<td><?php echo $value['firstname'];?></td>
+						<td><?php echo $value['lastname'];?></td>
+						<td><?php echo $value['email'];?></td>
+						<td><?php echo $value['address'] . ' '. $value['postal_code'] . ' ' . $value['city'];?></td>
+						<td><?php echo $value['phone'];?></td>
+						<td>
+							<select name="role" class="select-role" id="role-<?= $value['id']; ?>" >
+								<option value="user" <?php if(isset($value['role']) && $value['role'] == 'user'){ echo 'selected'; }?>>User</option>
+								<option value="admin" <?php if(isset($value['role']) && $value['role'] == 'admin'){ echo 'selected'; }?>>Admin</option>
+							</select>
+						</td>
+						<td>
+							<input type="checkbox" name="suppr" id="suppr-<?= $value['id']; ?>">
+						</td>
 
-</div>
-<?php
-/*}else{
-?>
+						<td>
+							<button type="submit" data-id="<?=$value['id'];?>">Appliquez</button>
+						</td>
+					</form>
+				</tr>
+				<?php }	?>
+			</tbody>
+		</table>
+
+	</div>
+	<?php
+}else{
+	?>
 	<script>
 		window.location='<?=$this->url('accueil');?>';
 	</script>
 	<?php
-}*/
+}
 ?>
 <?= $this->stop('main_content'); ?> 
 <?=$this->start('script'); ?>
