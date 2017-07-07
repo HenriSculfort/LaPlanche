37610aa9e345b681a6d47eeadd1e65e612d56f46
nlogeais <logeaisnicolas@yahoo.fr>
2017-07-07 14:31:09 +0200

    ajout description services

--- w/app/Views/default/index.php
+++ w/app/Views/default/index.php
@@ -3,10 +3,10 @@
 <?php $this->start('header_content') ?>
 
 <style> 
-  .intro-header
-  { 
-  	background-image: url(<?= $this->assetUrl('img/sport-ground.jpg')?>) 
-  }  
+    .intro-header
+    { 
+        background-image: url(<?= $this->assetUrl('img/sport-ground.jpg')?>) 
+    }  
 </style>
 
 
@@ -30,30 +30,30 @@
                 <h2 class="section-services">La planche vous permet de :</h2>
             </div>
         </div>
-        <div class="detail-services row text-center">
-            <div class="col-md-4">
+        <div class="services row text-center">
+            <div class="detail-services col-md-4">
                 <span class="fa-stack fa-4x">
-                 <img src="<?= $this->assetUrl('img/basketball.png')?>" alt="ballon de basket">
-             </span>
-             <h4 class="service-heading">Proposer un match</h4>
-             <p class="text-muted">En consultant le détail des terrains, il sera très simple pour vous de prévoir une rencontre. Il suffira de planifier la date à partir du terrain sélectionné.</p>
-         </div>
-         <div class="col-md-4">
-            <span class="fa-stack fa-4x">
-                <img src="<?= $this->assetUrl('img/trophy.png')?>" alt="coupe">
-            </span>
-            <h4 class="service-heading">Participer à un match</h4>
-            <p class="text-muted"></p>
-        </div>
-        <div class="col-md-4">
-            <span class="fa-stack fa-4x">
-            <img class="rotate" src="<?= $this->assetUrl('img/basketball-court.png')?>" alt="terrain de basket">
-            </span>
-            <h4 class="service-heading">Ajouter un terrain</h4>
-            <p class="text-muted">Nous </p>
+                    <img src="<?= $this->assetUrl('img/trophy.png')?>" alt="coupe">
+                </span>
+                <h4 class="service-heading">Participer à un match</h4>
+                <p class="text-muted">Envie d'un match de basket ? Consultez la liste des matches à proximité de chez vous. Vous pourrez échanger depuis le tchat avec la communauté pour organiser la rencontre !</p>
+            </div>
+            <div class="detail-services col-md-4">
+                <span class="fa-stack fa-4x">
+                    <img src="<?= $this->assetUrl('img/basketball.png')?>" alt="ballon de basket">
+                </span>
+                <h4 class="service-heading">Proposer un match</h4>
+                <p class="text-muted">En consultant le détail des terrains, il sera très simple pour vous de prévoir une rencontre. Il suffira de planifier la date à partir du terrain sélectionné.</p>
+            </div>
+            <div class="detail-services col-md-4">
+                <span class="fa-stack fa-4x">
+                    <img class="rotate" src="<?= $this->assetUrl('img/basketball-court.png')?>" alt="terrain de basket">
+                </span>
+                <h4 class="service-heading">Ajouter un terrain</h4>
+                <p class="text-muted">Vous connaissez un city qui n'est pas répertorié sur La Planche ? Rendez-vous sur votre espace personnel pour ajouter le terrain et ainsi programmer des rencontres avec les personnes de la communauté</p>
+            </div>
         </div>
     </div>
-</div>
 </section>
 
 <?php $this->stop('main_content') ?>
