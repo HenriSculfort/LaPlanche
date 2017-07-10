ed5437adbc5462dd2e78be24c4cb959d1a40e635
EBisbibi <etienne.braud@hotmail.fr>
2017-07-10 16:50:42 +0200

    protec admin

--- w/app/Controller/AdminGestionCompteController.php
+++ w/app/Controller/AdminGestionCompteController.php
@@ -10,91 +10,106 @@ class AdminGestionCompteController extends Controller
 
 	public function gestionCompte()
 	{
-		
-		$this->show('admin/compte');
+		if(!isset($_SESSION) || empty($_SESSION) || $_SESSION['user']['role'] != 'admin'){         
+			$this->show('w_errors/403');            
+		}
+		else {	
+
+			$this->show('admin/compte');
+		}
 	}
 
 
 	public function gestionCompteAjax()
 	{
-		if(isset($_GET['suppr']) && $_GET['suppr'] == 'off'){
-			$roleModif = [
-			'role' => $_GET['role'],
-			];
-
-			$UsersModel = new UsersModel();
-			$update = $UsersModel->update($roleModif, $_GET['id']);
-
-			if($update == true){
-
-				$json = [
-				'result' => true,
-				'message' => 'Le rôle de l\'utilisateur a été modifié'
-				];
-			}
-			
+		if(!isset($_SESSION) || empty($_SESSION) || $_SESSION['user']['role'] != 'admin'){         
+			$this->show('w_errors/403');            
 		}
-		elseif(isset($_GET['suppr']) && $_GET['suppr'] == 'on'){
-			$UsersModel = new UsersModel();
-			$suppr = $UsersModel->delete((int) $_GET['id']);
-
-			if($suppr == true){
-
-				$json = [
-				'result' => true,
-				'message' => 'L\'utilisateur a été supprimer'
+		else {	
+			if(isset($_GET['suppr']) && $_GET['suppr'] == 'off'){
+				$roleModif = [
+				'role' => $_GET['role'],
 				];
+
+				$UsersModel = new UsersModel();
+				$update = $UsersModel->update($roleModif, $_GET['id']);
+
+				if($update == true){
+
+					$json = [
+					'result' => true,
+					'message' => 'Le rôle de l\'utilisateur a été modifié'
+					];
+				}
+
 			}
+			elseif(isset($_GET['suppr']) && $_GET['suppr'] == 'on'){
+				$UsersModel = new UsersModel();
+				$suppr = $UsersModel->delete((int) $_GET['id']);
+
+				if($suppr == true){
+
+					$json = [
+					'result' => true,
+					'message' => 'L\'utilisateur a été supprimer'
+					];
+				}
+			}
+			$this->showJson($json);
 		}
-		$this->showJson($json);
 	}
 
 
 	public function getList()
 	{
+		if(!isset($_SESSION) || empty($_SESSION) || $_SESSION['user']['role'] != 'admin'){         
+			$this->show('w_errors/403');            
+		}
+		else {	
 
-		$UsersModel= new UsersModel();
-		$listuser = $UsersModel->findAll('username');
+			$UsersModel= new UsersModel();
+			$listuser = $UsersModel->findAll('username');
 
-		$html = '';
+			$html = '';
 
 		//on génére le formulaire en Ajax
-		foreach ($listuser as $key => $value) {
+			foreach ($listuser as $key => $value) {
 
-			$html .= '<tr>';
-			$html .= '<form type="GET" id="user-id-' . $value['id'] . '">';
-			$html .= '<td>' . $value['username'] . '</td>';
-			$html .= '<td>' . $value['level'] . '</td>';
-			$html .= '<td>' . $value['firstname'] . '</td>';
-			$html .= '<td>' . $value['lastname'] . '</td>';
-			$html .= '<td>' . $value['email'] . '</td>';
-			$html .= '<td>' . $value['address'] . ' '. $value['postal_code'] . ' ' . $value['city'] . '</td>';
-			$html .= '<td>' . $value['phone'] . '</td>';
-			$html .= '<td><select name="role" class="select-role" id="role-' . $value['id'] . '" >';
-			$html .= '<option value="user"';
-			if(isset($value['role']) && $value['role'] == 'user'){
-				$html .= 'selected';
+				$html .= '<tr>';
+				$html .= '<form type="GET" id="user-id-' . $value['id'] . '">';
+				$html .= '<td>' . $value['username'] . '</td>';
+				$html .= '<td>' . $value['level'] . '</td>';
+				$html .= '<td>' . $value['firstname'] . '</td>';
+				$html .= '<td>' . $value['lastname'] . '</td>';
+				$html .= '<td>' . $value['email'] . '</td>';
+				$html .= '<td>' . $value['address'] . ' '. $value['postal_code'] . ' ' . $value['city'] . '</td>';
+				$html .= '<td>' . $value['phone'] . '</td>';
+				$html .= '<td><select name="role" class="select-role" id="role-' . $value['id'] . '" >';
+				$html .= '<option value="user"';
+				if(isset($value['role']) && $value['role'] == 'user'){
+					$html .= 'selected';
+				}
+				$html .= '>User</option>';
+				$html .= '<option value="admin"';
+				if(isset($value['role']) && $value['role'] == 'admin'){ 
+					$html .= 'selected';
+				}
+				$html .= '>Admin</option>';
+				$html .= '</select></td><td>';
+				$html .= '<input type="checkbox" name="suppr" id="suppr-' . $value['id'] . '">';
+				$html .= '</td><td>';
+				$html .= '<button type="submit" data-id="' . $value['id'] . '" class="zob">Appliquez</button>';
+				$html .= '</td></form></tr>';
 			}
-			$html .= '>User</option>';
-			$html .= '<option value="admin"';
-			if(isset($value['role']) && $value['role'] == 'admin'){ 
-				$html .= 'selected';
-			}
-			$html .= '>Admin</option>';
-			$html .= '</select></td><td>';
-			$html .= '<input type="checkbox" name="suppr" id="suppr-' . $value['id'] . '">';
-			$html .= '</td><td>';
-			$html .= '<button type="submit" data-id="' . $value['id'] . '" class="zob">Appliquez</button>';
-			$html .= '</td></form></tr>';
+
+			$boucle = [
+			'html' => $html,
+			'listuser' => $listuser,
+			'id_user' => $value['id'],
+			'id' => $value['id']
+			];
+
+			$this->showJson($boucle);
 		}
-
-		$boucle = [
-		'html' => $html,
-		'listuser' => $listuser,
-		'id_user' => $value['id'],
-		'id' => $value['id']
-		];
-
-		$this->showJson($boucle);
 	}
 }
