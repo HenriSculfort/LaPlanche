3cf43d882f355921fdcb20197236d5833fc7565a
EBisbibi <etienne.braud@hotmail.fr>
2017-07-11 11:32:31 +0200

    placement boutons

--- w/app/Views/admin/courtsValidate.php
+++ w/app/Views/admin/courtsValidate.php
@@ -32,7 +32,7 @@ foreach($findAll as $court) {
 					<div class='col-md-2'>
 						<img class="img-rounded img-responsive" src="<?php if(isset($court['picture']) && !empty($court['picture'])){ echo $this->assetUrl('img/uploads/'.$court['picture']);} else{echo $this->assetUrl('img/court-default.png');}?>" alt='Le terrain'>
 					</div>
-					<div class='col-md-8'>
+					<div class='col-md-7'>
 						<h4><?= $court['name'];?></h4>
 						<p class="description-terrain"><?= nl2br($court['description']);?></p>
 						<br>
@@ -40,7 +40,7 @@ foreach($findAll as $court) {
 						<br>
 						<p class="description-terrain"><?= nl2br($court['opening_hours']);?></p>
 					</div>
-					<div class='col-md-2'>
+					<div class='col-md-3'>
 						<!--On envoie l'id du terrain que l'on veut valider ou supprimer avec un nom à chaque boutton qui devient un paramétre dans $_POST-->
 						<input type="hidden" name="valeurId" value="<?=$court['id'];?>">
 						<button type="submit" class='btn btn-success' name="validez">Valider</button>
